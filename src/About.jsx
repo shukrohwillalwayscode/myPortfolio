56adec1d9cfb36c0b8d54e4bf0363dc9a196d0dc
shukrohwillalwayscode <abdurraheemshukroh2018@gmail.com>
2026-05-31 09:18:03 +0100

    fix

--- src/About.jsx
+++ src/About.jsx
@@ -61,7 +61,7 @@ function About() {
             </p>
           </a>
           <a
-            href="/SHUKROH ABDURRAHEEM CV_RESUME .pdf"
+            href="/SHUKROH_ABDURRAHEEM CV_RESUME .pdf"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:scale-110 transition-transform duration-200"
